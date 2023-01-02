eff83c360031aa3106540c57b9fa24f5794f1924
Bob Chang <changxuanyuan@sina.com>
2023-01-02 21:29:34 +0800

    test add

--- frontend/static/js/views/Dashboard.js
+++ frontend/static/js/views/Dashboard.js
@@ -10,6 +10,7 @@ export default class extends AbstractView{
         return `
         <h1>welcome back,Bob</h1>
         <p>会当凌绝顶，一览众山小</p>
+        <p>长风破浪会有时，直挂云帆济沧海</p>
         <p>
           <a href="/posts" data-link>view recent posts</a>
         </p>
